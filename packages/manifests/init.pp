cce1161f895f5b97c02dadbc29684104f3854a05
Training <training@rrcs-172-254-254-146.nyc.biz.rr.com>
2022-08-26 16:37:38 +0530

    first commit

---
+++ packages/manifests/init.pp
@@ -0,0 +1,19 @@
+class packages {
+ 
+ package{'curl':
+   ensure => 'present',
+ }
+
+ service {
+   ensure => 'running',
+   enable => true,
+   
+ }
+
+ file { 'index.html':
+   ensure => 'present',
+   path => '~/project/index.html',
+   content => "<html><h1>welcome to DevOps training</h1></html>"
+ }
+
+}
